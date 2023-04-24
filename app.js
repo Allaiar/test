d1e7786f6fe7194dc5672e57579b4eeaeba9b323
allaiar <radzhapov.allayar@gmail.com>
2023-04-24 21:37:13 +0600

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const API_KEY = "https://rickandmortyapi.com/api/character";
 
+getMult();
 function getMult() {
   fetch(API_KEY)
     .then((res) => res.json())
     .then((res) => showMults(res))
     .catch((error) => console.log(error));
 }
-getMult();
 
 function getClassByRate(rate) {
   if (rate == "Dead") {
@@ -19,33 +19,32 @@ function getClassByRate(rate) {
 function showMults(data) {
   let elMults = document.querySelector(".cards");
   console.log(data);
-  data.results.map((mult) => {
-    let elMult = document.createElement("div");
-    elMult.classList.add("movie");
-    elMult.innerHTML = `
+  data.results
+    .filter((mult) => mult.gender === "Male")
+    .map((mult) => {
+      let elMult = document.createElement("div");
+      elMult.classList.add("card");
+      elMult.innerHTML = `
           <div class="card">
           <img class="img" src="${mult.image}" alt="">
                 <div class="card-text">
                     <h1>${mult.name}</h1>
                     <div class="point">
                         <div class="mult__average mult__average--${getClassByRate(
-                            mult.status
-                          )}"></div>
+                          mult.status
+                        )}"></div>
                         <p>${mult.status}</p>
                         <p>${mult.species}</p>
                     </div>
                     <p>Last known location:</p>
                     <p></p>
                     <p>First seen in:</p>
+                    <div>${mult.gender}</div>
                 </div>
           </div>
           `;
 
-    elMults.append(elMult);
-    document.querySelector(".cards").append(elMult);
-
-  });
+      elMults.append(elMult);
+      document.querySelector(".cards").append(elMult);
+    });
 }
-showMults();
-
-let gender = gender.filter(results => results.map == 'Human')
